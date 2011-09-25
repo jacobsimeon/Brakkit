6d050b343ecb8c892b6cae69f888e49586a79f58
Jacob Morris <jacob.s.morris@gmail.com>
2011-09-25 00:37:54 -0600

    finished redirection to user home page if signed in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    render :layout => 'brackets', :action => 'brackets' and return if user_signed_in?
+  end
+  def brackets
   end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,9 +1,9 @@
 module HomeHelper
   def session_link
     if user_signed_in?
-      link_to "Sign Out", destroy_user_session_path, :method => 'delete', :class => "session"
+      link_to "Sign Out", destroy_user_session_url, :class => "session"
     else
-      link_to "Sign In", new_user_session_path, :class => "session"
+      link_to "Sign In", new_user_session_url, :class => "session"
     end
   end
 end
