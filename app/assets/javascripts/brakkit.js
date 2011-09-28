58eaa8b6967c625a6900080a5a25a5726ca7c6d6
Jacob Morris <jacob.s.morris@gmail.com>
2011-09-27 19:14:14 -0600

    require files in correct order

--- app/assets/javascripts/brakkit.js
+++ app/assets/javascripts/brakkit.js
@@ -1,5 +1,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require ./libs/sproutcore
-//= require_tree ./brakkit
+//= require ./brakkit/brakkit-app.js
+//= require_tree ./brakkit/models
+//= require_tree ./brakkit/controllers
+//= require_tree ./brakkit/views
+//= require ./brakkit/support.js
+//= require ./brakkit/main.js
 
